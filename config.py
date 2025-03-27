5ba02223ed7e522da7ccbfe25bdaf85b36b0aa1b
O Vazio <vitor.devx@gmail.com>
2025-03-27 15:44:30 -0300

    Adicionando config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Arquivo para armazenar configurações do bot
+
+TOKEN = "MTM1NDU3ODc1MDU4NjQ5MTIyNQ.G6vOpZ.KHRSt0tVxkHxX22Idp3hsZf22EU3Ey3UYBjAG8"  # Substitua pelo token do seu bot
+PREFIX = "#"  # Prefixo dos comandos
